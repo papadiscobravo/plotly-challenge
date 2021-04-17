b356450d6b6c1be968974e90e41f2729b51dd604
pbernhardt <papadiscobravo@users.noreply.github.com>
2021-04-17 15:17:21 -0500

    bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -15,6 +15,43 @@ console.log("Data Promise: ", dataPromise);
 
 function DrawBarGraph(sampleId){
     console.log(`DrawBarGraph(${sampleId})`);
+
+    d3.json("samples.json").then(data => {
+        console.log(data);
+
+        // there's an error down here
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        // there's an error up there
+        console.log(resultArray);
+        var result = resultArray[0];
+        console.log(result);
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        console.log(otu_ids);
+        console.log(otu_labels);
+        console.log(sample_values);
+
+        yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`); // TBD
+        var barData = {
+            x: sample_values.slice(0,10),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10),
+            orientation: "h"
+        }
+
+        var barData = {
+            x: sample_values.slice(0,10),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10),
+            orientation: "h"
+        }
+
+    });
+
 };
 
 function DrawBubbleChart(sampleId){
